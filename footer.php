6f32bdd87c015b4ca2f46dc7c2065b5bb3ced4cb
Hugo <hug0alex@hotmail.com>
2015-06-09 16:32:22 -0300

    alteração no rodapé
    adicionadas colunas. ;)

--- footer.php
+++ footer.php
@@ -1,4 +1,40 @@
-<footer class="page-footer "><center>2015 © Todos os direitos reservados.</center></footer>    
+<footer class="page-footer ">
+  <div class="container">
+      <div class="row">
+          <div class="col-xs-4">      
+              <h4>Por <strong>Cristo</strong>, Nosso Senhor. <strong>Amém!</strong></h4>
+          </div>
+          <div class="col-xs-3"><h4>Contato e informações</h4></div>
+          <div class="col-xs-3"><h4>Siga-nós</h4></div>
+          <div class="col-xs-2"><h4>Por:</h4></div>
+      </div> 
+         
+      <div class="row">
+          <div class="col-xs-4">
+              <a href="" class="media-left">
+                  <figure><img src="http://placehold.it/100x100" alt="..."></figure>
+              </a>
+              <p class="media-body">Familia Catolica Trindade Santa</br>Todos os direitos reservados.</p>
+          </div>
+          <div class="col-xs-3">
+              <ul>
+                  <li><a href="">contato</a></li>
+                  <li><a href="">sobre</a></li>
+                  <li><a href="">termos de uso</a></li>
+              </ul>
+          </div>
+          <div class="col-xs-3">
+              <ul>
+                  <li><a href="">Facebook</a></li>
+                  <li><a href="">Twitter</a></li>
+                  <li><a href="">Instagram</a></li>
+                  <li><a href="">Youtube</a></li>
+              </ul>
+          </div>
+          <div class="col-xs-2"><figure><img src="http://placehold.it/60x60" alt="..."></figure></div>
+      </div>
+  </div>
+</footer>    
    <!-- scripts -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
    <script src="javascripts/bootstrap.min.js"></script>
